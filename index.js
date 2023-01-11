d1acb1be158c1ae1e55efe705a00d8f31806be10
chenp6 <cpr40517@gmail.com>
2023-01-12 00:36:28 +0800

    【deploy】 20230112

--- index.js
+++ index.js
@@ -2,34 +2,16 @@ import express, { json } from 'express'; //載入express框架模組
 import cors from 'cors';
 let app = express();
 
-app.use(cors({
-    origin: '*',
-}));
+// Middlewares
+app.use(express.json());
 
-
-// const rule = new schedule.RecurrenceRule();
-// rule.hour = 16; //台灣(+8)
-// rule.minute = 0;
-// rule.second = 50;
-// const updateEveryDay = schedule.scheduleJob(rule, function() {
-//     console.log("進行每日更新");
-//     NYCU.updateAllReserveProcess();
-//     console.log("完成每日更新");
-// });
-
-
-app.listen(3000, () => {
-    console.log(new Date() + "開始監聽port 3000!");  
+// Routes
+app.use('/src',express.static('./src'));
+app.get('/home',(req,res)=>{
+    let str = "src/index.html";  //引號內放入要切換的html檔案頁面
+    res.sendFile(str);
 });
-app.get("/getUserRank", async(req, res) => {
-    const schoolId = req.query.schoolId;
-    const userId = req.query.userId;
-    const groupNo = req.query.groupNo;
 
-    res.send({
-        index: schoolId,
-        rank: userId,
-        status: groupNo
-    });
-    return;
-});
+// connection
+const port = process.env.PORT || 9001;
+app.listen(port, () => console.log(`Listening to port ${port}`));
