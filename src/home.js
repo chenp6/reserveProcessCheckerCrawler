e9c7b2972ae62908da2cbbf3750081d58632b2a7
chenp6 <cpr40517@gmail.com>
2023-01-12 02:34:00 +0800

    【deploy】home

--- src/home.js
+++ src/home.js
@@ -2,8 +2,10 @@ import express from 'express'; //載入express框架模組
 const router = express.Router();
 
 router.get("/", async(req, res, next) => {
-    let str = "index.html"; //引號內放入要切換的html檔案頁面
-    return res.sendFile(str, { root: './src' }); // {root:'裡面html資料的資料夾相對位址'}
+    return res.status(200).json({
+        title: "Express Testing",
+        message: "The app is working properly!",
+    });
 });
 
 export default router
