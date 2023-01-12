05249c8b9bbf0df7089817c63c07db451d326e65
chenp6 <cpr40517@gmail.com>
2023-01-12 19:10:19 +0800

    【deploy】

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express, { json } from 'express'; //載入express框架模組
+import * as NCKU from './src/NCKUReserveProcess.js';
+import * as NYCU from './src/NYCUReserveProcess.js';
+import schedule from 'node-schedule';
 import cors from 'cors';
-import home from "./src/home.js";
 
 
 let app = express();
@@ -9,13 +11,14 @@ let app = express();
 app.use(express.json());
 
 // Routes
-app.use(express.static('public'))
-app.get("/", async(req, res, next) => {
+// app.use(express.static('public'))
+app.get("/getUserRank", async(req, res, next) => {
     return res.status(200).json({
         title: "Express Testing",
         message: "The app is working properly!",
     });
 });
+
 // connection
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
