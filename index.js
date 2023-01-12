8e633ae549d46f65c5bb0566ab3b9c94b449023c
chenp6 <cpr40517@gmail.com>
2023-01-12 15:57:58 +0800

    【deploy】 add html

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express, { json } from 'express'; //載入express框架模組
 import cors from 'cors';
 import home from "./src/home.js";
+import * as homepage from './src/index.html';
+
 
 let app = express();
 
@@ -8,7 +10,10 @@ let app = express();
 app.use(express.json());
 
 // Routes
-app.use("/home", home);
+app.get("/home",(req,res)=>{
+    let str = "index.html";  //引號內放入要切換的html檔案頁面
+    res.sendFile(str,{root:'.'});// {root:'裡面html資料的資料夾相對位址'}
+});
 
 app.get("/", async(req, res, next) => {
     return res.status(200).json({
