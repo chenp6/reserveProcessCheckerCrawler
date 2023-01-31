9c235ac2b0b83a5cff12dc6d94ba0b76c73122f9
chenp6 <cpr40517@gmail.com>
2023-01-31 19:00:44 +0800

    【local】(update) 上傳local測試檔

--- index_local.js
+++ index_local.js
@@ -4,30 +4,32 @@ import cors from 'cors';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-
+let connectStatus = "closed";
 
 let examTable;
 let groupTable;
 let processTable;
-const uri =
-    `mongodb+srv://${process.env.GUEST_USER}:${process.env.GUEST_PWD}@cluster0.lkdsifs.mongodb.net/?retryWrites=true&w=majority`;
+const uri = process.env.MONGODB_URL;
 
 // Create a new MongoClient
 const client = new MongoClient(uri);
 
 async function run() {
+    connectStatus = "start connect";
+
     // Connect the client to the server (optional starting in v4.7)
     await client.connect();
 
+    connectStatus = "start Establish and verify connection";
     // Establish and verify connection
     const db = client.db("reserveProcess");
     await db.command({ ping: 1 });
-    console.log("Connected successfully to server");
 
-
+    connectStatus = "start table collection connection";
     examTable = await db.collection("exam", { tls: true });
     groupTable = await db.collection("group", { tls: true });
     processTable = await db.collection("process", { tls: true });
+    connectStatus = "ok";
     console.log(new Date() + "資料庫資料完成連接")
 }
 run().catch(console.dir);
@@ -54,7 +56,19 @@ app.get("/getGroupSelect", async(req, res) => {
     return res.status(200).json(await groupTable.find({ school: req.query.school, examNo: '' + req.query.examNo }).toArray());
 });
 
+
+app.get("/getReserveProcess", async(req, res) => {
+    //group list
+    return res.status(200).json(await groupTable.findOne({ school: req.query.school, examNo: '' + req.query.examNo, groupNo: '' + req.query.groupNo }));
+});
+
+
 app.get("/getUserRank", async(req, res) => {
     //user rank
     return res.status(200).json(await processTable.findOne({ groupId: req.query.groupId, userId: req.query.userId }));
+});
+
+app.get("/getStatus", async(req, res) => {
+    //user rank
+    return res.status(200).json({ status: connectStatus })
 });
