19266481192e7e7b81cddd3ab12f27bf206a1c1e
chenp6 <cpr40517@gmail.com>
2023-02-04 14:40:01 +0800

    只留下Crawler資料

--- index.js
+++ index.js
@@ -1,103 +1,84 @@
-import express from 'express'; //載入express框架模組
 import { MongoClient } from "mongodb";
-import cors from 'cors';
+import { connectionMap, setConnections, updateTable, getAllObjects } from "./Component/Utils.js";
+import * as CCU from './Component/CCUReserveProcess.js';
+import * as NYCU from './Component/NYCUReserveProcess.js';
+import * as NCKU from './Component/NCKUReserveProcess.js';
+import * as NCU from './Component/NCUReserveProcess.js';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-let connectStatus = "closed";
-
-let examTable;
-let groupTable;
-let processTable;
+// Connection URI
 const uri = process.env.MONGODB_URL;
 
 // Create a new MongoClient
 const client = new MongoClient(uri);
 
 async function run() {
-    connectStatus = "start connect";
-
     // Connect the client to the server (optional starting in v4.7)
     await client.connect();
 
-    connectStatus = "start Establish and verify connection";
     // Establish and verify connection
     const db = client.db("reserveProcess");
     await db.command({ ping: 1 });
+    console.log("Connected successfully to server");
+    console.log(new Date() + "開始更新各校系組資料")
 
-    connectStatus = "start table collection connection";
-    examTable = await db.collection("exam", { tls: true });
-    groupTable = await db.collection("group", { tls: true });
-    processTable = await db.collection("process", { tls: true });
-    connectStatus = "ok";
-    console.log(new Date() + "資料庫資料完成連接")
+    await setConnections(db);
+
+
+    await updateExams(db);
+
+    //get all exams
+    await getAllObjects("exam")
+
+
+    await CCU.init();
+    await NYCU.init();
+    await NCKU.init();
+    await NCU.init();
+
+    console.log(new Date() + "完成更新!")
+
 }
 run().catch(console.dir);
 
-let app = express();
+async function updateExams() {
+    await updateCCUExams();
+    await updateNYCUExams();
+    await updateNCKUExams();
+    await updateNCUExams();
 
-
-app.use(cors({
-    origin: '*',
-}));
-
-
-app.listen(3000 || process.env.PORT, () => {
-    console.log(new Date() + "開始監聽port 3000!");
-});
-
-app.get("/getExamSelect", async(req, res) => {
-    try {
-        const examList = await examTable.find({ school: req.query.school }).toArray();
-        return res.status(200).json(examList);
-    } catch (error) {
-        return res.status(500).json({
-            result: null
-        })
+    async function updateCCUExams() {
+        // await updateTable("exam", { school: "CCU", examNo: '1' }, { name: "111學年度碩士班招生考試" });
+        // await updateTable("exam", { school: "CCU", examNo: '2' }, { name: "111學年度博士班招生考試" });
+        await updateTable("exam", { school: "CCU", examNo: '3' }, { name: "112學年度碩士班甄試" });
+        // await updateTable("exam", { school: "CCU", examNo: '4' }, { name: "111學年度數位學習碩士專班招生考試" });
+        // await updateTable("exam", { school: "CCU", examNo: '5' }, { name: "111學年度碩士專班招生考試" });
     }
 
-});
-
-app.get("/getGroupSelect", async(req, res) => {
-    //group list
-    try {
-        return res.status(200).json(await groupTable.find({ school: req.query.school, examNo: '' + req.query.examNo }).toArray());
-    } catch (error) {
-        return res.status(500).json({
-            result: null
-        })
+    async function updateNYCUExams() {
+        await updateTable("exam", { school: "NYCU", examNo: '112E' }, { name: "陽明校區博士班甄試" });
+        await updateTable("exam", { school: "NYCU", examNo: '112A' }, { name: "陽明校區碩士班甄試" });
+        await updateTable("exam", { school: "NYCU", examNo: '112' }, { name: "交大校區碩博班甄試" });
     }
-});
 
-app.get("/getReserveProcess", async(req, res) => {
-    //group list
-    try {
-        return res.status(200).json(await groupTable.findOne({ school: req.query.school, examNo: '' + req.query.examNo, groupNo: '' + req.query.groupNo }));
-    } catch (error) {
-        return res.status(500).json({
-            result: null
-        })
+    async function updateNCKUExams() {
+        await updateTable("exam", { school: "NCKU", examNo: '1' }, { name: "碩士班甄試" });
+        await updateTable("exam", { school: "NCKU", examNo: 'O' }, { name: "博士班甄試" });
+        await updateTable("exam", { school: "NCKU", examNo: 'H' }, { name: "寒假轉學甄試" });
     }
-});
 
-app.get("/getUserRank", async(req, res) => {
-    //user rank
-    try {
-        return res.status(200).json(await processTable.findOne({ groupId: req.query.groupId, userId: req.query.userId }));
-    } catch (error) {
-        return res.status(500).json({
-            result: null
-        })
+    async function updateNCUExams() {
+        await updateTable("exam", { school: "NCU", examNo: '142' }, { name: "112學年度碩士班、博士班甄試入學招生" });
     }
-});
+}
 
-app.get("/getStatus", async(req, res) => {
-    //user rank
-    try {
-        return res.status(200).json({ status: connectStatus })
-    } catch (error) {
-        return res.status(500).json({
-            result: null
-        })
-    }
-});
+async function getUserRank(db, groupId, userId) {
+    const table = await connectionMap.get('process');
+    return await table.findOne({ groupId: groupId, userId: userId });
+}
+
+async function getGroupProcess(db, school, examNo, groupNo) {
+    const table = await connectionMap.get('group');
+    return await table.findOne({ school: school, examNo: examNo, groupNo: groupNo });
+}
