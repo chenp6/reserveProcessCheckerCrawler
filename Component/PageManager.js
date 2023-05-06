7533fdef8e9949e493ed937ea87e715b04310e42
chenp6 <cpr40517@gmail.com>
2023-05-06 23:49:05 +0800

    【NKNU】(update) 新增成大

--- Component/PageManager.js
+++ Component/PageManager.js
@@ -50,6 +50,66 @@ export class NYCUPageManager {
 }
 
 
+export class NKNUPageManager {
+
+    constructor(page) {
+        this.page = page;
+    }
+
+    async selectExamSystem(systemValue) {
+        const typeSelector = 'select#ctl00_phMain_uSystem';
+
+        //已被選擇，則不用再select
+        const selectedValue = await this.page.$eval(typeSelector + " option[selected='selected']", element => element.getAttribute("value"))
+        if (selectedValue == systemValue) return;
+
+        await this.page.select(typeSelector, systemValue); // 按下按鈕
+        try {
+            await this.page.waitForNavigation();
+        } catch (e) {
+            if (e instanceof TimeoutError) {
+                await this.page.reload();
+            }
+        }
+    }
+
+    async selectExamType(typeValue) {
+        const typeSelector = 'select#ctl00_phMain_uType';
+
+        //已被選擇，則不用再select
+        const selectedValue = await this.page.$eval(typeSelector + " option[selected='selected']", element => element.getAttribute("value"))
+        if (selectedValue == typeValue) return;
+
+        await this.page.select(typeSelector, typeValue); // 按下按鈕
+        try {
+            await this.page.waitForNavigation();
+        } catch (e) {
+            if (e instanceof TimeoutError) {
+                await this.page.reload();
+            }
+        }
+    }
+
+    async selectExamList(groupVal) {
+        const typeSelector = 'select#ctl00_phMain_uProgram';
+
+        //已被選擇，則不用再select
+        const selectedValue = await this.page.$eval(typeSelector + " option[selected='selected']", element => element.getAttribute("value"))
+        if (selectedValue == groupVal) return;
+
+        await this.page.select(typeSelector, groupVal); // 按下按鈕
+        try {
+            await this.page.waitForNavigation();
+        } catch (e) {
+            if (e instanceof TimeoutError) {
+                await this.page.reload();
+            }
+        }
+    }
+
+
+}
+
 
 
 export async function navigateToPage(page, url) {
